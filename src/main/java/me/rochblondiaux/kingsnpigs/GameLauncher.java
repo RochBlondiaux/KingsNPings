88bdcc6c09537be36fe0de9855655c0b43be7bf3
Roch Blondiaux <contact@roch-blondiaux.com>
2024-09-08 16:59:06 +0200

    initial: project setup

---
+++ src/main/java/me/rochblondiaux/kingsnpigs/GameLauncher.java
@@ -0,0 +1,8 @@
+package me.rochblondiaux.kingsnpigs;
+
+public class GameLauncher {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/me/rochblondiaux/kingsnpigs/KingsNPigs.java
@@ -0,0 +1,13 @@
+package me.rochblondiaux.kingsnpigs;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class KingsNPigs {
+
+    public KingsNPigs() {
+        log.info("KingsNPigs is starting...");
+
+    }
+
+}
